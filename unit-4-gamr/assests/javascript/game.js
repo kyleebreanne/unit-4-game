cc596e974e04b65fdf6c62278117b8849e51adff
kyleebreanne <44128719+kyleebreanne@users.noreply.github.com>
2018-11-26 18:39:10 -0700

    Add files via upload

---
+++ unit-4-gamr/assests/javascript/game.js
@@ -0,0 +1,13 @@
+// winning number, travler score and win & loss
+var winningNumber = Math.floor(Math.random() * 300) + 30;
+var travelScore = 0;
+var van = Math.floor(Math.random() * 80) + 30;
+var road = Math.floor(Math.random() * 10) + 0;
+var cabin = Math.floor(Math.random() * 30) + 9;
+var lake = Math.floor(Math.random() * 50) + 20;
+
+console.log(lake)
+// loop for the numbers
+// jquery images for the crsytals
+// Assign each picture a random number
+//if/then win/lose
